38d13ebe1afb152e67d4618470b72a15fc400b13
bharatmk257 <bharatmk257@gmail.com>
2019-01-20 23:41:16 +0530

    written first function

--- IntroToFunction/bin/main.dart
+++ IntroToFunction/bin/main.dart
@@ -2,4 +2,20 @@ import 'package:IntroToFunction/IntroToFunction.dart' as IntroToFunction;
 
 main(List<String> arguments) {
   print('Hello world: ${IntroToFunction.calculate()}!');
+  doSomething();
+}
+
+doSomething() {
+  print('hello functions');
+  sayMyName();
+}
+
+sayMyName() {
+  print('hello my name');
+  var name = "bharat";
+  if (name.contains("b")) {
+    print('hey');
+  } else {
+    print('nay');
+  }
 }
