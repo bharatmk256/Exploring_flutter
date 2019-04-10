8a1c950fb1aaded518b0e3fac3d105f3dcdf0f34
bharatmk257 <bharatmk257@gmail.com>
2019-04-10 11:43:30 +0530

    project done

--- parsing_data/lib/main.dart
+++ parsing_data/lib/main.dart
@@ -7,6 +7,9 @@ import 'package:http/http.dart' as http;
 void main() async {
   List _data = await getJson();
   print(_data[0]["title"]);
+  for (var i = 0; i < _data.length; i++) {
+    print("Data: ${_data[i]["title"]}");
+  }
   runApp(
     new MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -15,13 +18,54 @@ void main() async {
           title: Text("Json Parsing"),
           backgroundColor: Colors.redAccent,
         ),
-        body: Center(
-          child: Text(
-            _data[0]["title"]
-          ),
+        body: ListView.builder(
+          itemCount: _data.length,
+          itemBuilder: (BuildContext context, int position) {
+            return Column(
+              children: <Widget>[
+                Divider(
+                  height: 13.4,
+                ),
+                ListTile(
+                  title: Text("${_data[position]["title"]}"),
+                  subtitle: Text("${_data[position]["body"]}"),
+                  leading: CircleAvatar(
+                    // radius: 30.0,
+                    backgroundColor: Colors.redAccent,
+                    child: Text(
+                      _data[position]["title"][0],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  onTap: () => showTapMessage(context, _data[position]["title"]),
+                )
+              ],
+            );
+          },
         ),
       ),
     ),
+  );
+}
+
+void showTapMessage(BuildContext context, String message) {
+  var alertDialog = new AlertDialog(
+    title: Text(message),
+    actions: <Widget>[
+      FlatButton(
+        onPressed: () => Navigator.of(context).pop(),
+        child: Text("Ok"),
+      ),
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (context) {
+      return alertDialog;
+    },
   );
 }
 
